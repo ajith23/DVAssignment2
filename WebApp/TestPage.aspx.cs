3f65b9f7c824c6f0c1480b9eba2f645991ec2bac
unknown <avimalch@EN4110028214B.fulton.ad.asu.edu>
2016-10-09 17:56:52 -0700

    initial version

---
+++ WebApp/TestPage.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class TestPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //UtilityLocal.TransformFile(@"c:\users\avimalch\documents\visual studio 2015\Projects\AWAssignment1\WebApp\data\class_click.csv",
+            //    @"c:\users\avimalch\documents\visual studio 2015\Projects\AWAssignment1\WebApp\data\class_click_transformed.csv");
+            //UtilityLocal.CleanTransformedFile(@"c:\users\avimalch\documents\visual studio 2015\Projects\AWAssignment1\WebApp\data\class_click_transformed.csv", @"c:\users\avimalch\documents\visual studio 2015\Projects\AWAssignment1\WebApp\data\class_click_transformed_cleaned.csv");
+
+
+        }
+    }
+}
